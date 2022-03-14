9df7dc3dee4f8504a62c1204e9e62ab2264175ab
yan <294145891@qq.com>
2022-03-14 10:33:46 +0800

    实现项目列表基本功能（表格，删除功能）

--- Myapp/models.py
+++ Myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class DB_project(models.Model):
+    name = models.CharField(max_length=30,null=True,blank=True)
+    creater = models.CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return '项目名称' + self.name
--- Myapp/views.py
+++ Myapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
+from Myapp.models import *
 
 # Create your views here.
+# 进入项目列表
+def project_list(request):
+    project = DB_project.objects.all()
+    return render(request, 'project_list.html', {"projects": project})
+
+# 删除项目
+def del_project(request, pid):
+    # print(pid)
+    DB_project.objects.filter(id=pid).delete()
+    return HttpResponseRedirect('/project_list/')
